44be4b59ec5bc20602c872459f4593dbf94899df
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-12-05 22:11:52 +0300

    ДЗ готово

---
+++ Homeworks/HW_6/main.py
@@ -0,0 +1,191 @@
+from typing import List
+import databases
+import sqlalchemy
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from sqlalchemy import ForeignKey
+
+DATABASE_URL = "sqlite:///mydatabase.db"
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+users = sqlalchemy.Table(
+    "users",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String(32)),
+    sqlalchemy.Column("surname", sqlalchemy.String(32)),
+    sqlalchemy.Column("email", sqlalchemy.String(128)),
+    sqlalchemy.Column("password", sqlalchemy.String(16)),
+)
+
+products = sqlalchemy.Table(
+    "products",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String(32)),
+    sqlalchemy.Column("description", sqlalchemy.String(128)),
+    sqlalchemy.Column("price", sqlalchemy.Float())
+)
+
+orders = sqlalchemy.Table(
+    "orders",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("id_user", ForeignKey("users.id"), nullable=False),
+    sqlalchemy.Column("id_product", ForeignKey("products.id"), nullable=False),
+    sqlalchemy.Column("date", sqlalchemy.String(32)),
+    sqlalchemy.Column("status", sqlalchemy.Boolean),
+)
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+metadata.create_all(engine)
+
+app = FastAPI()
+
+
+class UserIn(BaseModel):
+    name: str = Field(max_length=32)
+    surname: str = Field(max_length=32)
+    email: str = Field(max_length=128)
+    password: str = Field(max_length=16)
+
+
+class User(BaseModel):
+    id: int
+    name: str = Field(max_length=32)
+    surname: str = Field(max_length=32)
+    email: str = Field(max_length=128)
+    password: str = Field(max_length=16)
+
+
+class ProductIn(BaseModel):
+    name: str = Field(max_length=32)
+    description: str = Field(max_length=128)
+    price: float = Field()
+
+
+class Product(BaseModel):
+    id: int
+    name: str = Field(max_length=32)
+    description: str = Field(max_length=128)
+    price: float = Field()
+
+
+class OrderIn(BaseModel):
+    id_user: int
+    id_product: int
+    date: str = Field(max_length=32)
+    status: bool
+
+
+class Order(BaseModel):
+    id: int
+    id_user: int
+    id_product: int
+    date: str = Field(max_length=32)
+    status: bool
+
+
+@app.post("/users/", response_model=User)
+async def create_user(user: UserIn):
+    query = users.insert().values(**user.dict())
+    last_record_id = await database.execute(query)
+    return {**user.dict(), "id": last_record_id}
+
+
+@app.post("/products/", response_model=Product)
+async def create_product(product: ProductIn):
+    query = products.insert().values(**product.dict())
+    last_record_id = await database.execute(query)
+    return {**product.dict(), "id": last_record_id}
+
+
+@app.post("/orders/", response_model=Order)
+async def create_order(order: OrderIn):
+    query = orders.insert().values(**order.dict())
+    last_record_id = await database.execute(query)
+    return {**order.dict(), "id": last_record_id}
+
+
+@app.get("/users/", response_model=List[User])
+async def read_users():
+    query = users.select()
+    return await database.fetch_all(query)
+
+
+@app.get("/users/{user_id}", response_model=User)
+async def read_user(user_id: int):
+    query = users.select().where(users.c.id == user_id)
+    return await database.fetch_one(query)
+
+
+@app.get("/products/", response_model=List[Product])
+async def read_products():
+    query = products.select()
+    return await database.fetch_all(query)
+
+
+@app.get("/products/{product_id}", response_model=Product)
+async def read_product(product_id: int):
+    query = products.select().where(products.c.id == product_id)
+    return await database.fetch_one(query)
+
+
+@app.get("/orders/", response_model=List[Order])
+async def read_orders():
+    query = orders.select()
+    return await database.fetch_all(query)
+
+
+@app.get("/orders/{order_id}", response_model=Order)
+async def read_product(order_id: int):
+    query = orders.select().where(orders.c.id == order_id)
+    return await database.fetch_one(query)
+
+
+@app.put("/users/{user_id}", response_model=User)
+async def update_user(user_id: int, new_user: UserIn):
+    query = users.update().where(users.c.id == user_id).values(**new_user.dict())
+    await database.execute(query)
+    return {**new_user.dict(), "id": user_id}
+
+
+@app.put("/products/{product_id}", response_model=Product)
+async def update_product(product_id: int, new_product: ProductIn):
+    query = products.update().where(products.c.id == product_id).values(**new_product.dict())
+    await database.execute(query)
+    return {**new_product.dict(), "id": product_id}
+
+
+@app.put("/orders/{order_id}", response_model=Order)
+async def update_order(order_id: int, new_order: OrderIn):
+    query = orders.update().where(orders.c.id == order_id).values(**new_order.dict())
+    await database.execute(query)
+    return {**new_order.dict(), "id": order_id}
+
+
+@app.delete("/users/{user_id}")
+async def delete_user(user_id: int):
+    query = users.delete().where(users.c.id == user_id)
+    await database.execute(query)
+    return {'message': 'User deleted'}
+
+
+@app.delete("/products/{product_id}")
+async def delete_product(product_id: int):
+    query = products.delete().where(products.c.id == product_id)
+    await database.execute(query)
+    return {'message': 'Product deleted'}
+
+
+@app.delete("/orders/{order_id}")
+async def delete_order(order_id: int):
+    query = orders.delete().where(orders.c.id == order_id)
+    await database.execute(query)
+    return {'message': 'Order deleted'}
